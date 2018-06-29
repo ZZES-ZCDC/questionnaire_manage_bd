632b41ab2fd4e0659e933b424db3f3580cefb69d
zzes <kuanglong0312@sina.com>
2018-06-29 20:46:31 +0800

    fix: #13  表格数据存在问题解决，数据库操作问题

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: "eslint-config-egg",
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  globals: {
+    tinymce: true,
+    qcVideo: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': 0,
+    'space-before-function-paren': [1, {
+      'anonymous': 'always',
+      'named': 'never'
+    }],
+    'semi': 0
+  }
+}
+
+// http://eslint.org/docs/user-guide/configuring
--- app/controller/question.js
+++ app/controller/question.js
@@ -7,15 +7,42 @@ class QuestionController extends Controller {
    * 录入问卷信息
    */
   async addData() {
-    const {ctx} = this
+    const {
+      ctx
+    } = this
     const question = ctx.request.body
     ctx.validate({
-      method: { type: 'string', required: true },
-      wish: { type: 'boolean', required: true },
-      before: { type: 'boolean', required: true },
-      phone: { type: 'number', required: true },
-      content: { type: 'string', required: true },
+      method: {
+        type: 'string',
+        required: true
+      },
+      wish: {
+        type: 'boolean',
+        required: true
+      },
+      before: {
+        type: 'boolean',
+        required: true
+      },
+      phone: {
+        type: 'number',
+        required: true
+      },
+      content: {
+        type: 'string',
+        required: true
+      },
     }, question)
+    if (question.wish === true) {
+      question.wish = '是'
+    } else {
+      question.wish = '否'
+    }
+    if (question.before === true) {
+      question.before = '是'
+    } else {
+      question.before = '否'
+    }
     const result = await ctx.service.question.create(question)
     ctx.helper.success({
       ctx,
@@ -27,10 +54,15 @@ class QuestionController extends Controller {
    * 渲染获取所有问卷数据
    */
   async getAllData() {
-    const {ctx} = this
-    const {page, limit} = ctx.query
+    const {
+      ctx
+    } = this
+    const {
+      page,
+      limit
+    } = ctx.query
     let token = ctx.cookies.get('token')
-    if(token){
+    if (token) {
       const result = await ctx.curl(`http://localhost:7001/question/page?page=${page}&limit=${limit}`, {
         // 必须指定 method
         method: 'GET',
@@ -45,9 +77,9 @@ class QuestionController extends Controller {
       const count = await ctx.service.question.getAllDataNum()
       ctx.body = {
         code: 0,
-        count:count,
-        msg:"",
-        data:result.data.data
+        count: count,
+        msg: "",
+        data: result.data.data
       }
     }
   }
@@ -56,10 +88,21 @@ class QuestionController extends Controller {
    * 获取所有问卷数据(有分页)
    */
   async getAllDataByPage() {
-    const {ctx} = this
-    const {page, limit} = ctx.query
-    let offset = page
-    const result = await ctx.service.question.getAllDataByPage(offset, limit)
+    const {
+      ctx
+    } = this
+    const {
+      page,
+      limit
+    } = ctx.query
+    let thePage = parseInt(page)
+    if (thePage === 1) {
+      thePage = thePage
+    } else {
+      thePage = thePage + parseInt(limit) - 1
+    }
+
+    const result = await ctx.service.question.getAllDataByPage(thePage, limit)
     ctx.helper.success({
       ctx,
       res: result
